b10a0309455265d72b5ab30a9c341cbf3b136327
Chinonsobit <chinonsoamos22@gmail.com>
2025-12-16 14:32:06 +0100

    AMAP website
    Website for grooming Macroeconomic Analysts

---
+++ types.ts
@@ -0,0 +1,18 @@
+export interface TeamMember {
+  name: string;
+  role: string;
+  email: string;
+  image: string;
+  bio: string;
+}
+
+export interface NavItem {
+  label: string;
+  path: string;
+}
+
+export interface CurriculumModule {
+  title: string;
+  description: string;
+  topics: string[];
+}
